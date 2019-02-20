d4ae178ed02bd604e55bb93acc46e5d8a4777e01
Femi Adebowale <feadebowale@gmail.com>
2019-02-20 00:45:55 -0500

    Instantiate Projectile

--- Assets/Code/Slingshot.cs
+++ Assets/Code/Slingshot.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     void Awake(){
         Transform launchPointTrans = transform.Find("LaunchPoint"); 
         //searches for slingshot named launchpoint and returns its transform (position, rotation, scale)
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
 
     void OnMouseEnter() {
@@ -20,6 +28,17 @@ public class Slingshot : MonoBehaviour {
     void OnMouseExit() {
         print("Slingshot:OnMouseExit()"); //detects that we are out of sphere collider
         launchPoint.SetActive(false); //removes it from being active in the game, deactivates halo
+    }
+
+    void OnMouseDown(){
+        //  The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        //Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //Start it at LaunchPoint
+        projectile.transform.position = launchPos;
+        //Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 
 }
